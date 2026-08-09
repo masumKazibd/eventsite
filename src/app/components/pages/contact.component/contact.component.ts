3be2b2c974e2f61f81b759e3a64815456fd349f8
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-08-09 12:13:43 +0600

    faq and schedule design upgrade

--- src/app/components/pages/contact.component/contact.component.ts
+++ src/app/components/pages/contact.component/contact.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  onSubmit(event: Event) {
+    event.preventDefault();
+    alert('Thanks for reaching out! (This is a dummy form)');
+  }
+}
